cfecb045b2bdd5dc06cdb580209df17b7847baf9
andrewpatterson3001 <andrew.patterson.3001@gmail.com>
2016-10-04 22:10:54 -0700

    Add label, button, and change button text through an action.

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = "An interesting Fact..."
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        funFactLabel.text = "Another Interesting Fact!"
+    }
 
 }
 
